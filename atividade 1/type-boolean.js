5cd47d00e48070b9c5e5487106a3e83c9972bb25
leonardoMDA <162999522+leonardoMDA@users.noreply.github.com>
2024-04-18 08:32:09 -0300

    Add files via upload

---
+++ atividade 1/type-boolean.js
@@ -0,0 +1,20 @@
+// bolean
+// true -> verdadeiro
+// false -> falso
+const primeironumero = 5;
+const segundonumero = 5;
+let cadastroAtivado = true;
+
+//comparação de variaveis com operadores
+
+console.log(primeironumero === segundonumero);// igual
+console.log(primeironumero != segundonumero);//diferente
+console.log(primeironumero < segundonumero);//menor
+console.log(primeironumero > segundonumero);//maior
+console.log(primeironumero <= segundonumero);//menor ou igual
+console.log(primeironumero >= segundonumero);//maior ou igual
+
+const texto1 = "Alura";
+const texto2 = "alura";
+
+console.log(texto1 === texto2)
---
+++ atividade 1/type-string.js
@@ -0,0 +1,19 @@
+//Tipo String
+
+const texto1 = "Olá, mundo!";
+const texto2 = "Olá, mundo!";
+const senha = "senha@Supersegura456!";
+const StringDeNumeros = "34567";
+
+const citacao = "meu nome é ";
+const meuNome = "Leonardo";
+
+// concatenação (+)
+
+console.log(texto1)
+console.log(texto2)
+console.log(senha)
+console.log(StringDeNumeros)
+console.log(citacao + meuNome)
+
+//template'string OU template literal
