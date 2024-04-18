55200cade4c6cc991cec28330480b7737e2c9802
leonardoMDA <162999522+leonardoMDA@users.noreply.github.com>
2024-04-18 08:39:05 -0300

    Add files via upload

---
+++ atividade 2/type-number.js
@@ -0,0 +1,21 @@
+//Tipo Number
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+const terceiroNumero = 2.1;
+
+let soma = primeiroNumero + segundoNumero;
+let subtracao = primeiroNumero - segundoNumero;
+let multiplicacao = primeiroNumero * segundoNumero;
+let divisao = primeiroNumero / segundoNumero;
+
+console.log("A soma e",soma)
+console.log("A subtração é",subtracao)
+console.log("A multiplação é", multiplicacao)
+console.log("A divisão é", divisao,"\n")
+
+//----------------
+
+
