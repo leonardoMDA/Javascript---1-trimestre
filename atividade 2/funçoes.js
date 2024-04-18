5bfc8f500a38f843681a9af6c89ef98b8997fd96
leonardoMDA <162999522+leonardoMDA@users.noreply.github.com>
2024-04-18 08:31:50 -0300

    Add files via upload

---
+++ atividade 2/funçoes.js
@@ -0,0 +1,38 @@
+function imprimirTexto(texto){
+    console.log(texto)
+}
+
+imprimirTexto("olá")    
+imprimirTexto("oi")
+
+function texto(text){
+    return text;
+}
+console.log(texto("parametro"))
+
+function soma(numero1,numero2){
+    return numero1 + numero2;
+}
+
+
+function subtracao(numero1,numero2){
+    return numero1 - numero2;
+}
+
+
+function multiplicacao(numero1,numero2){
+    return numero1 * numero2;
+}
+
+
+function divisao(numero1,numero2){
+    return numero1 / numero2;
+}
+
+console.log("a soma dos numeros é", soma(3,2))
+
+console.log("a subtração dos numeros é", subtracao(3,2))
+
+console.log("a multiplicação dos numeros é", multiplicacao(3,2))
+
+console.log("a divisão dos numeros é", divisao(3,2))
