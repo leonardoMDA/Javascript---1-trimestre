5500e077f412906faa35a0835d69ababc553f725
leonardoMDA <162999522+leonardoMDA@users.noreply.github.com>
2024-04-18 08:27:14 -0300

    Add files via upload

---
+++ ativadade 3/desafio3.js
@@ -0,0 +1,12 @@
+const notas = [10,6,8,5.5,10]
+
+console.log(notas) 
+
+notas.pop();
+
+console.log(notas)
+
+const media = (notas[0] + notas[1] + notas[3])/ notas.length;
+
+console.log(`a media é ${media}.`)
+
