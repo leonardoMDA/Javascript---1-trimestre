a6dd667205c1fbfb2019601c53ff2186bc3e9397
leonardoMDA <162999522+leonardoMDA@users.noreply.github.com>
2024-05-08 12:07:37 -0300

    Add files via upload

---
+++ ativadade 3/desafio5.js
@@ -0,0 +1,6 @@
+const nomes = ["João", "Ana", "Caio", "Lana", "Marjonie", "Leo"]
+
+nomes.splice(1,2, "Rodrigo") 
+//nomes.push("Rodrigo")
+
+console.log(nomes)
---
+++ ativadade 3/desafio6.js
@@ -0,0 +1,9 @@
+const salaJS = ["Evaldo", "Caamis", "Mari"];
+const salaPython = ["Ju", "Leo", "Raquel"];
+
+const SalaUnificadas = salaJS.concat(salaPython);
+const SalaUnificadas2 = salaPython.concat(salaJS)
+
+
+console.log(SalaUnificadas)
+console.log(SalaUnificadas2)
