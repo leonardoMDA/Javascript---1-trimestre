c87f027670e3aa3450015b9c3ccff1d4676f3dda
leonardoMDA <162999522+leonardoMDA@users.noreply.github.com>
2024-05-15 11:51:18 -0300

    Add files via upload

---
+++ ativadade 3/media-for-of.js
@@ -0,0 +1,11 @@
+const notas = [10, 6.5, 8, 7.5];
+
+for(let indice = 0; indice <notas.length; i++){
+
+    somaDasNotas += notas(i);
+    console.log(someDasNotas)
+};
+    
+const media = somaDasNotas / notas.length;
+
+console.log(`A média das notas é ${media}`)
---
+++ ativadade 3/media-for.js
@@ -0,0 +1,12 @@
+const notas = [10, 6.5, 8, 7.5]
+
+let somaDasNotas = 0;
+
+for(let i = 0; i < notas.lenght; i++ ) {
+
+    somaDasNotas += notas[1];
+};
+
+const media = somaDasnotas / notas.lenght;
+
+console.log()
