3b3a019d751f56b6bb16426db089b26a9c0e9be5
leonardoMDA <162999522+leonardoMDA@users.noreply.github.com>
2024-05-22 12:08:32 -0300

    Add files via upload

---
+++ ativadade 3/callback.js
@@ -0,0 +1,15 @@
+const nomes = ["Daniel","LarissaXP","Kamis"]
+
+nomes.forEach(function(nome){
+    console.log(nome);
+});
+
+nomes.forEach(function(nome){
+    console.log(nome);
+});
+
+function imprimirNome(nome){
+    console.log(nome);
+};
+
+nomes.forEach(imprimirNome);
